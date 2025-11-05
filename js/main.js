33a892958998434d3681e6d13bf05d68edc153ac
nhnn3106 <120386489+nhnn3106@users.noreply.github.com>
2025-11-05 20:54:00 +0700

    p1

--- js/main.js
+++ js/main.js
@@ -1,9 +1,21 @@
 import { partnerLogoBasePath, partnerLogos, productList } from "./data.js";
 
+// nav
+$(function () {
+  // hide show nav
+  $(".navbar").hidescroll();
+
+  const toggleBtn = $("#toggle_btn");
+  const dropdownMenu = $(".dropdown-menu");
+
+  toggleBtn.click(function () {
+    dropdownMenu.toggleClass("open");
+  });
+});
+
+// partner-logo
 $(function () {
   const container = document.getElementById("partner-logo-list");
-
-  // partner-logo
   partnerLogos.forEach((logo) => {
     const img = document.createElement("img");
     img.src = partnerLogoBasePath + logo.fileName;
